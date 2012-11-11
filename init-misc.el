a92ae86fb217c1324b9d88fdaec537bdb1127e52
bamanzi <bamanzi@localhost>
2012-11-11 17:13:30 +0800

    split init.el into multiple files

---
+++ init-misc.el
@@ -0,0 +1,16 @@
+;;***  tabbar
+(eval-after-load "tabbar"
+  `(progn
+     (tabbar-mode t)
+     (define-key tabbar-mode-map (kbd "<C-tab>")     'tabbar-forward-tab)
+     (define-key tabbar-mode-map (kbd "<C-S-tab>")   'tabbar-backward-tab)
+     (define-key tabbar-mode-map (kbd "<C-M-tab>")   'tabbar-forward-group)
+     (define-key tabbar-mode-map (kbd "<C-S-M-tab>") 'tabbar-backward-group)
+
+     (define-key tabbar-mode-map (kbd "<f12> <right>") 'tabbar-forward)
+     (define-key tabbar-mode-map (kbd "<f12> <left>")  'tabbar-backward)
+     (define-key tabbar-mode-map (kbd "<f12> <up>")    'tabbar-press-home)
+
+     (if (display-graphic-p)
+         (require 'tabbar-ruler nil t))  
+     ))
