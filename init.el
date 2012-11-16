b06d83b563c6abf9143a09490f906946c843edeb
bamanzi <bamanzi@gmail.com>
2012-11-16 22:02:54 +0800

    add {e,}sh-toggle.el

--- init.el
+++ init.el
@@ -9,12 +9,19 @@
   )
 
 
-(let ((this_dir (if load-file-name
+(setq dotemacs-elite-dir (if load-file-name
                     (file-name-directory load-file-name)
-                  default-directory)))
-  (let ((default-directory this_dir))
-    (add-to-list 'load-path this_dir)
-    (normal-top-level-add-subdirs-to-load-path))
+                  default-directory))
+(progn
+  (add-to-list 'load-path dotemacs-elite-dir)
+  (mapc #'(lambda (file)
+            (when (and (file-directory-p file)
+                       (not (file-exists-p (concat file "/.nosearch"))))
+              (message "Prepending %s to load-path" file)
+              (add-to-list 'load-path file)
+              (let ((default-directory file))
+                (normal-top-level-add-subdirs-to-load-path))))
+        (directory-files dotemacs-elite-dir 'full "[^\\.]+"))
 
   (setq idle-require-idle-delay 10
         idle-require-load-break 1)
@@ -27,8 +34,9 @@
 ;;	    (unless (ignore-errors          
                   (load-file file))
 ;;          (message "Failed %s" file)))
-	(directory-files this_dir 'full "^init-\.*.el$"))
+	(directory-files dotemacs-elite-dir 'full "^init-\.*.el$"))
 
   (if (fboundp 'idle-require-mode)
       (idle-require-mode 1)))
+      
       
