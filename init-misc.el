5b79a84bc1de47af707ff65be1f44278208ff759
bamanzi <bamanzi@gmail.com>
2012-11-28 21:40:30 +0800

    change tabbar-mode keybindings

--- init-misc.el
+++ init-misc.el
@@ -102,8 +102,8 @@
 (eval-after-load "tabbar"
   `(progn
      (tabbar-mode t)
-     (define-key tabbar-mode-map (kbd "<C-tab>")     'tabbar-forward-tab)
-     (define-key tabbar-mode-map (kbd "<C-S-tab>")   'tabbar-backward-tab)
+     (define-key tabbar-mode-map (kbd "<C-tab>")     'tabbar-forward)
+     (define-key tabbar-mode-map (kbd "<C-S-tab>")   'tabbar-backward)
      (define-key tabbar-mode-map (kbd "<C-M-tab>")   'tabbar-forward-group)
      (define-key tabbar-mode-map (kbd "<C-S-M-tab>") 'tabbar-backward-group)
 
