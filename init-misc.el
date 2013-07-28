750603822ecaa225e8531e9f35ce5ff247973852
bamanzi <bamanzi@localhost>
2013-07-28 15:55:53 +0800

    defun window-toggle-split-direction

--- init-misc.el
+++ init-misc.el
@@ -461,6 +461,40 @@ That is, a string used to represent it on the tab bar."
 
 (global-set-key (kbd "<f5> s")  'anything-yasnippet-2)
 
+
+;;** windows
+(defun window-toggle-split-direction ()
+  "Switch window split from horizontally to vertically, or vice versa.
+
+i.e. change right window to bottom, or change bottom window to right."
+  (interactive)
+  (require 'windmove)
+  (let ((done))
+    (dolist (dirs '((right . down) (down . right)))
+      (unless done
+        (let* ((win (selected-window))
+               (nextdir (car dirs))
+               (neighbour-dir (cdr dirs))
+               (next-win (windmove-find-other-window nextdir win))
+               (neighbour1 (windmove-find-other-window neighbour-dir win))
+               (neighbour2 (if next-win (with-selected-window next-win
+                                          (windmove-find-other-window neighbour-dir next-win)))))
+          ;;(message "win: %s\nnext-win: %s\nneighbour1: %s\nneighbour2:%s" win next-win neighbour1 neighbour2)
+          (setq done (and (eq neighbour1 neighbour2)
+                          (not (eq (minibuffer-window) next-win))))
+          (if done
+              (let* ((other-buf (window-buffer next-win)))
+                (delete-window next-win)
+                (if (eq nextdir 'right)
+                    (split-window-vertically)
+                  (split-window-horizontally))
+                (set-window-buffer (windmove-find-other-window neighbour-dir) other-buf))))))))
+
+(global-set-key (kbd "<f11> |") 'window-toggle-split-direction)
+
+
+(setq ediff-window-setup-function 'ediff-setup-windows-plain)
+
 ;;** misc
 
 ;;info+.el: more colors (and other enhancements) 
@@ -468,6 +502,3 @@ That is, a string used to represent it on the tab bar."
   `(require 'info+)
   )
 
-
-
-(setq ediff-window-setup-function 'ediff-setup-windows-plain)
