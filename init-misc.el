899d9ce820b103d889d2fc96e2a098773a32b918
bamanzi <bamanzi@localhost>
2014-12-28 21:59:49 +0800

    expose command htmlize-{buffer,file,region}

--- init-misc.el
+++ init-misc.el
@@ -907,3 +907,11 @@ vi style of % jumping to matching brace."
         (linum-mode 1)
         (goto-line (read-number "Goto line: ")))
     (linum-mode -1)))
+
+;;--
+(autoload 'htmlize-buffer "htmlize"
+  "Convert BUFFER to HTML, preserving colors and decorations." t)
+(autoload 'htmlize-file   "htmlize"
+  "Load FILE, fontify it, convert it to HTML, and save the result." t)
+(autoload 'htmlize-region "htmlize"
+  "Convert the region to HTML, preserving colors and decorations." t)
